1e09c488b4f16a3c7fd9a0abd37c4cde271083b0
Vinayak <vinayakkaushikk@gmail.com>
2017-03-15 11:54:02 +0530

    database added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 var mustacheExpress = require('mustache-express');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
+
+// Postgres
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.use(express.static(__dirname + '/public'));
 
