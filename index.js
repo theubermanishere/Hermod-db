28e3d41ecb415690aa436fb9e08628a31a9b23c6
Vinayak <vinayakkaushikk@gmail.com>
2017-03-15 12:11:34 +0530

    added nodemailer; still needs to be tested

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var mustacheExpress = require('mustache-express');
 var pg = require('pg');
+var nodemailer = require('nodemailer');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,6 +17,32 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
     .on('row', function(row) {
       console.log(JSON.stringify(row));
     });
+});
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'hermoddb@gmail.com',
+        pass: 'arnoldbuddha280'
+    }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: '"Hermod" <hermoddb@gmail.com>', // sender address
+    to: '', // list of receivers
+    subject: '', // Subject line
+    text: 'Hello world ?', // plain text body
+    html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
 });
 
 app.use(express.static(__dirname + '/public'));
